c6ffab1a68f0c1780418f0dc4cecec97e3c34313
RaviThakor <ravit@zignuts.com>
2023-01-18 11:24:21 +0530

    commit all data

--- database/migrations/2023_01_18_054213_create_role__users_table.php
+++ database/migrations/2023_01_18_054213_create_role__users_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('role__users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('role__users');
     }
 };
